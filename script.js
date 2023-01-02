311c64c7e93d4752e042be9d35ad4b81c98f9526
Nic Ortiz <ortiz.nagare@gmail.com>
2023-01-01 23:26:35 -0500

    updated card design with toggle icons, adjusted announcements/trending

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const notificationBell = document.getElementById("notifications");
 
 notificationBell.addEventListener("click", () => {
     notificationBell.classList.toggle("fa-bell");
+    notificationBell.classList.toggle("fas");
     notificationBell.classList.toggle("fa-bell-on");
+    notificationBell.classList.toggle("far");
 });
 
 
@@ -12,6 +14,14 @@ profileIcon.addEventListener("click", () => {
     profileIcon.classList.toggle("fas");
     profileIcon.classList.toggle("fad");
 })
+
+const clickableIcons = Array.from(document.querySelectorAll("i.no-select"));
+
+clickableIcons.forEach(each => each.addEventListener("click", () => {
+    each.classList.toggle("fas");
+    each.classList.toggle("far");
+    console.log(each.textContent);
+}))
 
 
 // clearButton.addEventListener("click", () => {
